b64169ff3cbed3351c9ed654dd66d26cc3814bdd
Mariam Khan <52761858+mariam1203@users.noreply.github.com>
2024-05-28 00:55:33 -0400

    Project Setup + Header Finished
    - setup project and initial files done
    - finished header for both desktop and mobile
    
    [following along with JS Mastery tut]

---
+++ src/components/Button.jsx
@@ -0,0 +1,29 @@
+import ButtonSvg from "../assets/svg/ButtonSvg";
+
+const Button = ({ className, href, onClick, children, px, white }) => {
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-[#D9B4F6] ${
+    px || "px-7"
+  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+
+  const spanClasses = "relative z-10";
+
+  const renderButton = () => (
+    <button className={classes} onClick={onClick}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </button>
+  );
+
+  const renderLink = () => (
+    <a href={href} className={classes}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </a>
+  );
+
+  return href // if href is provided, render a link
+    ? renderLink()
+    : renderButton(); // else, render a button
+};
+
+export default Button;
